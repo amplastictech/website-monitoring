71e59b70e1eb897897646ad44eb45660be1b3deb
amplastictech <admin@americanplastictech.com>
2025-03-13 17:04:50 -0700

    Update check_jeep_page.py

--- check_jeep_page.py
+++ check_jeep_page.py
@@ -4,28 +4,25 @@ import os
 
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
 
 driver = webdriver.Firefox(options=options)
 
 try:
-    url = "https://www.kbdbodykits.com/jeep-wrangler-jk-flat-polyurethane-flat-fender-flares-kit"
-    driver.get(url=url)
+    driver.get("https://www.kbdbodykits.com/jeep-wrangler-jk-flat-polyurethane-flat-fender-flares-kit")
 
     expected_text = "Jeep Wrangler JK (2/4 Doors) & Unlimited 2007-2018 Front & Rear 4 Piece Polyurethane Fender Flares Kit"
-    now = datetime.datetime.now()
 
     if expected_text not in driver.page_source:
-        os.makedirs("404", exist_ok=True)
+        now = datetime.datetime.now()
         filename = now.strftime("%Y-%m-%d_%H-%M-%S_jk.txt")
-        filepath = os.path.join("404", filename)
 
-        with open(filepath, "w") as file:
+        os.makedirs("404", exist_ok=True)
+        with open(os.path.join("404", filename), "w") as file:
             file.write("JK flares page not loaded")
 
-        print(f"Specified text NOT FOUND. Logged in {filename}")
+        print(f"Specified text NOT found! Logged in {filename}")
     else:
-        print("Specified text found! No log created.")
+        print("Specified text found, no issues.")
 
 except Exception as e:
     print("An error occurred:", e)
